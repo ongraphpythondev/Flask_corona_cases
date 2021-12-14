beebbe5c7f61f7cd1d2332ea01854f2a950881ef
unknown <riteshpandey1200@gmail.com>
2021-12-14 18:48:18 +0530

    update

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask , render_template , request
 import urllib.request, json
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 app = Flask(__name__)
 url = "https://api.rootnet.in/covid19-in/stats/latest"
+url_cases = "https://api.covid19api.com/country/india"
 
 # this is for routing
 @app.route("/" )
@@ -15,15 +17,21 @@ def index():
         dict = json.loads(data)
         datetime = dict["lastRefreshed"].split("T")
         time = datetime[1].split(".")[0]
-        states = list()
-        cases = list()
-        for state in dict["data"]["regional"]:
-            states.append(state["loc"])
-            cases.append(state["confirmedCasesIndian"])
+
+        Confirmed = list()
+        Date = list()
+
+        response = urllib.request.urlopen(url_cases)
+        data = response.read()
+        dict2 = json.loads(data)
+        for data in dict2:
+            Confirmed.append(data["Confirmed"])
+            var = data["Date"].split("T")
+            Date.append(var[0])
         
-        x = states
-        y = cases
-        plt.barh(x, y)
+
+        plt.plot(Date, Confirmed, label = "Total Cases")
+        plt.legend()
         plt.savefig('static/image/pic.png')
         return render_template("index.html" , data = dict["data"] , date= datetime[0] , time = time)
 
